aca145d0bba41c74730d0eaa55f5ae7ab1676cda
chrisfauries <45318711+chrisfauries@users.noreply.github.com>
2018-12-08 15:17:25 -0600

    finished consolidating code, adding function for square and square-root

--- calc.js
+++ calc.js
@@ -1,5 +1,7 @@
 window.onload = function(){
     
+    
+            ///// Key Press Targets /////
     var calculator = document.getElementById("calculator");
     var keys = calculator.querySelector(".keys");
     keys.addEventListener('click', function(e){
@@ -13,42 +15,40 @@ window.onload = function(){
         }else if(e.target.attributes.type.value === "negative"){
             negativePressed(e.target.innerHTML);
             
-    }else if(e.target.attributes.type.value === "reset"){
+        }else if(e.target.attributes.type.value === "reset"){
             clear();
         
+        }else if(e.target.attributes.type.value === "squared"){
+            squaredPressed();
+            
+        }else if(e.target.attributes.type.value === "square-root"){
+            squareRootPressed();    
     }});
- 
-    
+     
             ///// Display Variables /////
-    
     var displayActive = document.getElementById("active-display");
     var displayExpression = document.getElementById("expression-display");
     var displayAnswer = document.getElementById("answer-display");
     
-
-    
-    
             /////Operator Variables/////
-        
     var decimal = document.getElementById("decimal");
     var division = document.getElementById("division");
     var multiplication = document.getElementById("multiplication");
     var subtraction = document.getElementById("subtraction");
     var addition = document.getElementById("addition");
     var equals = document.getElementById("equals");
-    var testButton = document.getElementById("test-button");
+    
+            /////Square Variables/////
+    var squared = document.getElementById("square");
+    var squareRoot = document.getElementById("square-root");
     
     
-            /////Initial State/////
-    
+            /////Initial States/////
     multiplication.setAttribute("state","initial");
     division.setAttribute("state","initial");
     equals.setAttribute("state","initial");
-
     
             /////Number functions/////
-    
-    
     function digitPressedAttributeResetAfterEquals(){
             equals.classList.remove("pressed");
             displayExpression.innerHTML = "";
@@ -78,13 +78,11 @@ window.onload = function(){
             displayActive.innerHTML = displayActive.innerHTML + number;
             }  
     }
-
-
-    
+             /////decimal function/////
     decimal.onclick = function(){
         
         if(decimal.getAttribute("state","restrict")){
-            //nothing//
+            //do nothing//
             
         }else if(equals.className == "pressed"){
             decimal.setAttribute("state","restrict");
@@ -98,16 +96,23 @@ window.onload = function(){
         decimal.setAttribute("state","restrict");
         displayActive.style.display = "block";
         displayActive.innerHTML = displayActive.innerHTML + ".";
-            }
+        }
     }
     
-
+            /////Square/Square Root Functions/////
+    function squaredPressed(){
+            var value = displayAnswer.innerHTML;
+            var answer = eval(value * value);
+            displayAnswer.innerHTML = answer;
+    }
     
+    function squareRootPressed(){
+            var value = displayAnswer.innerHTML;
+            var answer = Math.sqrt(value);
+            displayAnswer.innerHTML = answer;
+    }
     
-    
-            /////Operator Functions/////
-    
-    
+            /////Operator Function/////
     function operatorPressed(operator){
         
         if(multiplication.getAttribute("state","initial") || division.getAttribute("state","initial")){
@@ -142,7 +147,7 @@ window.onload = function(){
         displayActive.innerHTML = displayActive.innerHTML + operator;
         }
     }
-        
+            /////Negative Function/////
     function negativePressed(operator){           
         
         if(subtraction.className == "pressed"){
@@ -168,9 +173,7 @@ window.onload = function(){
         }
     }
         
-        /////Expression Functions/////
-
-    
+        /////Expression Function/////
     function errorFlash(){
         displayAnswer.innerHTML = "Error: Divided by 0";
         displayAnswer.classList.add("flash"); 
@@ -181,9 +184,7 @@ window.onload = function(){
         displayAnswer.innerHTML = "";  
     }
 
-    
     equals.onclick = function(){
-        
         if(equals.getAttribute("state","initial")){
             //do nothing//
             
@@ -206,16 +207,14 @@ window.onload = function(){
         if(displayAnswer.innerHTML == Infinity){
             errorFlash();
             setTimeout(clearErrorFlash,5000); 
-
         }
     }
-    
+        /////Clear Function/////
     function clear(){
         location.reload(); 
     }
     
-        /////NumPad Interaction/////
-    
+        /////NumPad Interactions/////
     /*window.onkeypress = function(event) {
         if (event.keyCode == 97){
             digitPressed(1);
@@ -226,5 +225,4 @@ window.onload = function(){
 
 ///// Significant Figures /////
 ///// Add parathesis functionality /////
-///// Create funtions for "Square" and "Square Root" /////
 /////NumPad Interaction/////
